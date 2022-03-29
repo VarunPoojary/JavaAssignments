9210bf14cac365cfe38f4f72705690a1c7084328
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2022-03-29 12:58:06 +0530

    Create Assignment3.java

---
+++ Assignment3.java
@@ -0,0 +1,2 @@
+//Write a java program to create class car,truck and motorcycle which extends the vehicle class (attribute registration_number, color, type of vehicle) 
+//with their own attribute like make,CC and fuel type. Input data from the user and print all the details.
